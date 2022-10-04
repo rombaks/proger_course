75a7b0d8b54f3a4ea386a2e572fddae1910d2324
rombaks <rombaks.dev@gmail.com>
2022-10-04 12:09:31 +0500

    Init repo with my_site django-project

---
+++ mysite/news/views.py
@@ -0,0 +1,158 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
+from django.contrib import messages
+from django.contrib.auth import login, logout
+from django.core.mail import send_mail
+
+from .models import News, Category
+from .forms import NewsForm, UserRegisterForm, UserLoginForm, ContactForm
+from .utils import MyMixin
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Регистрация успешно завершена')
+            return redirect('home')
+        else:
+            messages.error(request, 'Ошибка регистрации')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'news/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+        # else:
+        #     messages.error(request, 'Ошибка авторизации')
+    else:
+        form = UserLoginForm()
+    return render(request, 'news/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+
+def test(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            mail = send_mail(form.cleaned_data['subject'], form.cleaned_data['content'],
+                             'mailprotocol@rambler.ru', ['blackshave@yandex.ru'], fail_silently=True)
+            if mail:
+                messages.success(request, 'Письмо отправлено')
+                return redirect('test')
+            else:
+                messages.error(request, 'Ошибка отправки')
+    else:
+        form = ContactForm()
+    return render(request, 'news/test.html', {'form': form})
+
+
+class HomeNews(MyMixin, ListView):
+    model = News  # заменяет news = News.objects.all() из index(). ссылка на шаблон $имя_модели$_list.html
+    template_name = 'news/home_news_list.html'  # замена стандартного news_list.html
+    context_object_name = 'news'  # замена стандартного object_list
+    # extra_context = {'title': 'Главная'}
+    mixin_prop = 'hello world'
+    paginate_by = 2
+
+    def get_context_data(self, **kwargs):
+        """ Возвращает контекст, в тч динамический """
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.get_upper('Главная страница')
+        context['mixin_prop'] = self.get_prop()
+        return context
+
+    def get_queryset(self):
+        """ Формирует запрос. В данном примере чтобы отображались только новости is_published=True"""
+        return News.objects.filter(is_published=True).select_related('category')
+
+
+class NewsByCategory(MyMixin, ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+    allow_empty = False
+    paginate_by = 2
+
+    def get_context_data(self, **kwargs):
+        """ Возвращает контекст, в тч динамический """
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.get_upper(Category.objects.get(pk=self.kwargs['category_id']))
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs['category_id'],
+                                   is_published=True).select_related('category')
+
+
+class ViewNews(DetailView):
+    model = News
+    # template_name = 'news/news_detail.html'
+    context_object_name = 'news_item'
+    # pk_url_kwarg = 'news_id'
+
+
+class CreateNews(LoginRequiredMixin, CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    # success_url = reverse_lazy('home')
+    # login_url = '/login/'
+    raise_exception = True
+
+# def index(request):
+# """Заменил на класс HomeNews"""
+#     news = News.objects.all()
+#     context = {
+#         'news': news,
+#         'title': 'Список новостей',
+#     }
+#     return render(request, template_name='news/index.html', context=context)
+
+
+# def get_category(request, category_id):
+#     news = News.objects.filter(category_id=category_id)
+#     category = Category.objects.get(pk=category_id)
+#     context = {
+#         'news': news,
+#         'category': category,
+#     }
+#     return render(request, template_name='news/category.html', context=context)
+
+
+# def view_news(request, news_id):
+#     # news_item = News.objects.get(pk=news_id)
+#     news_item = get_object_or_404(News, pk=news_id)
+#     context = {
+#         'news_item': news_item,
+#     }
+#     return render(request, template_name='news/view_news.html', context=context)
+
+
+# def add_news(request):
+#     if request.method == 'POST':
+#         form = NewsForm(request.POST)
+#         if form.is_valid():
+#             # print(form.cleaned_data)
+#             # news = News.objects.create(**form.cleaned_data)
+#             news = form.save()
+#             return redirect(news)
+#     else:
+#         form = NewsForm()
+#
+#     context = {'form': form}
+#     return render(request, template_name='news/add_news.html', context=context)
